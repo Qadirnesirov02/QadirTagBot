a864c2f901ecb5967d0ad2255df13d4f909ab152
Qadirnesirov <123129882+Qadirnesirov@users.noreply.github.com>
2023-02-01 17:19:38 +0400

    update pyrogram_modul.py

--- pyrogram_modul.py
+++ pyrogram_modul.py
@@ -16,11 +16,7 @@ app = Client(
 @app.on_message(filters.new_chat_members, group=1)
 async def hg(bot: Client, msg: Message):
     for new_user in msg.new_chat_members:
-        if str(new_user.id) == str(Config.BOT_ID):
-            await msg.reply(
-                f'''`Salam` {msg.from_user.mention} `məni` {msg.chat.title} `qrupuna əlavə etdiyiniz üçün təşəkkürlər🥰`**''')
-
-        elif str(new_user.id) == str(Config.OWNER_ID):
+        if str(new_user.id) == str(Config.OWNER_ID):
             await msg.reply('Sahibim indi qrupa qoşuldu😍\nxoş gəldin aramıza Sahibim, Necəsən?🥰')
 
             buttons = [[InlineKeyboardButton("➕ Qrupa Əlavə Et ➕",url="http://t.me/Rahid_Tag_Bot?startgroup=a"),
