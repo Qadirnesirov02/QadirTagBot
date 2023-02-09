cd3b51d1fc1881aaee9e5d914c302491aad51153
Qadirnesirov <123129882+Qadirnesirov@users.noreply.github.com>
2023-02-09 04:25:55 +0400

    update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config():
     API_ID = int(os.environ.get("API_ID", "19485442"))
     API_HASH = os.environ.get("API_HASH", "a03fcb372b3ec4e406b5d52f84b02e53")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5394785524:AAEozSRqSyD2vVtvVPgGgYr8ffj9PwReL2E")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "RahidRobot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Rahid_Tag_Bot")
     BOT_NAME = os.environ.get("BOT_NAME", "Rahid")
     BOT_ID = int(os.environ.get("BOT_ID", "5394785524"))
     SUDO_USERS = os.environ.get("SUDO_USERS", "571698989").split()
