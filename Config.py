9cddbc01cfbb4acc3084ba6010757ddb36d78e75
Qadirnesirov <123129882+Qadirnesirov@users.noreply.github.com>
2023-02-11 01:55:02 +0400

    update Config.py

--- Config.py
+++ Config.py
@@ -4,13 +4,13 @@ import os
 class Config():
     # Get these values from my.telegram.org
     # https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID", "19485442"))
-    API_HASH = os.environ.get("API_HASH", "a03fcb372b3ec4e406b5d52f84b02e53")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5394785524:AAEozSRqSyD2vVtvVPgGgYr8ffj9PwReL2E")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Rahid_Tag_Bot")
-    BOT_NAME = os.environ.get("BOT_NAME", "Rahid")
-    BOT_ID = int(os.environ.get("BOT_ID", "5394785524"))
-    SUDO_USERS = os.environ.get("SUDO_USERS", "571698989").split()
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Cenublar")
-    OWNER_ID = int(os.environ.get("OWNER_ID", "571698989"))
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Rahid_7")
+    API_ID = int(os.environ.get("API_ID", "26712413"))
+    API_HASH = os.environ.get("API_HASH", "3298034eb7cec614ef852fda02536153")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5925538079:AAEoh1spOKWdYPUJhxNeZ5ivLHk3OkqbsY0")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Qadir_Tager_Bot")
+    BOT_NAME = os.environ.get("BOT_NAME", "Qadir")
+    BOT_ID = int(os.environ.get("BOT_ID", "5925538079"))
+    SUDO_USERS = os.environ.get("SUDO_USERS", "5860341998").split()
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "imperator")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "5860341998"))
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "nesirovqadirofficiall")
